c2fda4c8544e2e0be6cfcbbd90b4057ed2667fa1
Ondřej Svojše <ondrejs1@ondrej-air.mshome.net>
2017-02-14 16:35:36 +0100

    Initial Commit

---
+++ RainyShinyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainyShinyApp
+//
+//  Created by Ondřej Svojše on 14.02.17.
+//  Copyright © 2017 Ondřej Svojše. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
